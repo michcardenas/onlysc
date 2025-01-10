37b30c75c0ae478bbe94e1e5b32c059e68690b0e
Samuel <sambarbosa66@gmail.com>
2025-01-10 16:11:56 -0500

    bla bla

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
     <meta name="robots" content="{{ $metaRobots ?? 'index,follow' }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 </head>
 
 <body>
