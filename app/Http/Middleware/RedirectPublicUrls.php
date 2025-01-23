452a5249cdc08e6a848b06cb74d76f62c3886d39
Samuel <sambarbosa66@gmail.com>
2025-01-23 12:02:51 -0500

    Public 4to intento

--- app/Http/Middleware/RedirectPublicUrls.php
+++ app/Http/Middleware/RedirectPublicUrls.php
@@ -2,13 +2,20 @@
 namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class RedirectPublicUrls
 {
     public function handle(Request $request, Closure $next)
     {
+        Log::info([
+            'full_url' => $request->fullUrl(),
+            'path' => $request->path(),
+            'is_public' => $request->is('public/*')
+        ]);
+        
         if ($request->is('public/*')) {
-            return redirect(substr($request->path(), 7));
+            return redirect($request->path());
         }
         return $next($request);
     }
